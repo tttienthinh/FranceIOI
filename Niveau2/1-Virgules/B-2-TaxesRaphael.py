edbd75c952e8dfb56d5e1fbda98109f59c7c7b8d
ttnice <tranthuongtienthinh@gmail.com>
2020-05-18 13:26:21 +0200

    Avec Raphael

---
+++ Niveau2/1-Virgules/B-2-TaxesRaphael.py
@@ -0,0 +1,16 @@
+taxe1=float(input())
+taxe2=float(input())
+prixtx=float(input())
+
+prixht=prixtx / (1+taxe1/100)
+
+prixttc=prixht * (1+taxe2/100)
+
+prixttc=prixttc*100
+
+prixttc=round(prixttc)
+
+prixttc=prixttc/100
+
+print(prixttc)
+print(0.2+0.1)
